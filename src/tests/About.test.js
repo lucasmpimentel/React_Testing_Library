fd920fd1d4693c8b9077fe239921cdec1f548709
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-02-21 19:36:51 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  test('O titulo About Pokédex aparece na pagina', () => {
+    renderWithRouter(<About />);
+    const aboutTitle = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstPTag = /This application simulates a Pokédex/i;
+    const secPTag = /One can filter Pokémons by type/i;
+    const getFirstPTag = screen.getByText(firstPTag);
+    const getSecPTag = screen.getByText(secPTag);
+    expect(getFirstPTag && getSecPTag).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const getImage = screen
+      .getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+    expect(getImage).toBeInTheDocument();
+  });
+});
